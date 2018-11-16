db185e23475a4302370a98640b9027de0698495a
Adebayo Adekunle <aadebayo@systemspecs.com.ng>
2018-11-16 14:39:46 +0100

    index.html and index.css completed

--- Public/scripts/main.js
+++ Public/scripts/main.js
@@ -2,21 +2,23 @@
     'use strict';
     /*jslint browser: true*/
     /*eslint-env browser*/
-    /*global $*/
 
-    /* code for menu color change on scroll */
+    /* code section for menu color change on scroll */
+
     $(document).scroll(function () {
         if ($(this).scrollTop() < $('#main-nav').height()) {
-            $('#main-nav').css('background-color', 'transparent');
-            $('#main-nav').find('.navbar-nav').removeClass('ml-auto');
-            $('#main-nav').css('box-shadow', 'none');
+            $('#main-nav').css({
+                'background-color': 'transparent',
+                'box-shadow': 'none'
+            });
         } else {
-            $('#main-nav').css('background-color', '#FFFFFF');
-            $('#main-nav').find('.navbar-nav').addClass('ml-auto');
-            $('#main-nav').css('box-shadow', '2px 0 5px rgba(0, 0, 0, 0.5)');
-
+            $('#main-nav').css({
+                'background-color': '#FFFFFF',
+                'box-shadow': '2px 0 5px rgba(0, 0, 0, 0.5)'
+            });
         }
     });
-    /* code for menu color change on scroll end */
+
+    /* code section for menu color change on scroll end */
 
 }());
