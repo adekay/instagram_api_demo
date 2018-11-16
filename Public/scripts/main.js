22d27b84fd7667cab61d41ccb112e769a8806fde
Adebayo Adekunle <aadebayo@systemspecs.com.ng>
2018-11-16 16:15:35 +0100

    API data fetching and video display complete

--- Public/scripts/main.js
+++ Public/scripts/main.js
@@ -1,9 +1,7 @@
 (function () {
     'use strict';
-    /*jslint browser: true*/
-    /*eslint-env browser*/
 
-    /* code section for menu color change on scroll */
+    /* ---------- code section for menu color change on scroll ---------- */
 
     $(document).scroll(function () {
         if ($(this).scrollTop() < $('#main-nav').height()) {
@@ -19,6 +17,63 @@
         }
     });
 
-    /* code section for menu color change on scroll end */
+    /* ---------- code section for menu color change on scroll END ---------- */
+
+
+    /* ---------- code section for fetching and displaying instagram videos ---------- */
+
+    // execute on page load
+    displayInstagramVideos(API_DETAILS.api, 0);
+
+    // recursive function that gets the APi tag data, filters for videos, displays and 
+    // then runs again to fectch the next batch of paginated data... till the total video count is 10,
+    // or there is no more data
+    function displayInstagramVideos(api, count) {
+
+        getInstagramApiData(api)
+            .then(filterForVideos)
+            .catch(handleError);
+
+        function filterForVideos(res) {
+            console.log('start');
+
+            $.each(res.data, function (index, data) {
+                console.log(count);
+                if (data.type === 'video') {
+                    addVideoToPage(data, count);
+                    count++;
+                }
+                // checks if the next batch of paginated data is needed to complete 10 videos, if so runs again with new next_url
+                if ((index + 1) === res.data.length && count < 10 && res.pagination.next_url) {
+                    console.log('entered');
+                    displayInstagramVideos(res.pagination.next_url, count);
+                }
+
+                // ensures number of videos doesn't exceed 10
+                return count < 10;
+            });
+            console.log(res);
+        }
+
+        function handleError(error) {
+            console.log(error.responseJSON.meta.error_message);
+        }
+    }
+
+    function getInstagramApiData(api) {
+        return $.getJSON(api);
+    }
+
+    function addVideoToPage(data, count) {
+        console.log(data);
+        var videoContainer = $('.video-container').eq(count);
+        videoContainer.find('.vid video').attr('src', data.videos.standard_resolution.url).attr('poster', data.images.standard_resolution.url);
+        videoContainer.find('.details .profile-pic').attr('src', data.user.profile_picture);
+        videoContainer.find('.details .User-name').text(data.user.full_name).attr('href', data.link);
+        videoContainer.find('.details .video-caption').text(data.caption.text);
+        videoContainer.find('.details .comment-button').text('comment').attr('href', data.link);
+    }
+
+    /* ---------- code section for fetchind and displaying instagram videos END ---------- */
 
 }());
